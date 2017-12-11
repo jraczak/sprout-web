1fa163cd9889e08c2934ee5e97abec009c34d869
Justin <jraczak@gmail.com>
2017-12-10 18:19:25 -0800

    Initial commit

---
+++ src/main/java/io/sproutmoney/sproutweb/SproutWebApplication.java
@@ -0,0 +1,12 @@
+package io.sproutmoney.sproutweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutWebApplication.class, args);
+	}
+}
